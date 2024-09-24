b5cd0d4a31c93a5123181d97ff386cf42e03011f
Florian Schlager <schlagerf22108@sus.htl-grieskirchen.at>
2024-09-24 15:15:57 +0200

    Normale ForEach Schleife erstellt und ausgeführt

--- LambdaHausuebung/src/HalloJavamitForEach.java
+++ LambdaHausuebung/src/HalloJavamitForEach.java
@@ -1,4 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class HalloJavamitForEach
 {
-    
+    List<String> list;
+
+    public HalloJavamitForEach ()
+    {
+        list = new ArrayList<>();
+        list.add("Begriff 1");
+        list.add("Begriff 2");
+        list.add("Begriff 3");
+    }
+
+    public void showList ()
+    {
+        for (String s : list) {
+            System.out.println(s);
+        }
+    }
+
 }
--- LambdaHausuebung/src/Main.java
+++ LambdaHausuebung/src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args)
     {
-
+        HalloJavamitForEach h1 = new HalloJavamitForEach();
+        h1.showList();
     }
 }
