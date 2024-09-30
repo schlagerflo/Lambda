c558483f609aff91d55ad7e4426cfac9f69b759e
Florian Schlager <schlagerf22108@sus.htl-grieskirchen.at>
2024-09-30 17:07:20 +0200

    Methode NumberTester fertiggestellt

--- LambdaHausuebung/src/NumberTester.java
+++ LambdaHausuebung/src/NumberTester.java
@@ -1,30 +1,48 @@
-public class NumberTester
-{
+import java.io.*;
+
+public class NumberTester {
     private NumberTest oddTester;
     private NumberTest primeTester;
     private NumberTest palindromeTester;
-    public NumberTester (String fileName)
-    {
-        //todo
+
+    BufferedReader reader;
+    public NumberTester (String fileName) {
+        try {
+            reader = new BufferedReader(new FileReader(fileName));
+        }catch (FileNotFoundException e){
+            System.out.println("File was not found!");
+        }
     }
 
-    public void setOddEvenTester (NumberTest oddTester)
-    {
+    public void setOddEvenTester (NumberTest oddTester) {
         this.oddTester = oddTester;
     }
 
-    public void setPrimeTester (NumberTest primeTester)
-    {
+    public void setPrimeTester (NumberTest primeTester) {
         this.primeTester = primeTester;
     }
 
-    public void setPalindromeTester (NumberTest palindromeTester)
-    {
+    public void setPalindromeTester (NumberTest palindromeTester) {
         this.palindromeTester = palindromeTester;
     }
 
-    public void testFile ()
-    {
-        //todo
+    public void testFile () {
+        try{
+            int tests = Integer.parseInt(reader.readLine());
+            for (int i = 0; i < tests; i++)
+            {
+                String[] parts = reader.readLine().split(" ");
+                int methodNum = Integer.parseInt(parts[0]);
+                int parameter = Integer.parseInt(parts[1]);
+
+                switch (methodNum){
+                    case 1: System.out.println(oddTester.testNumber(parameter) ? "ODD" : "EVEN");
+                    case 2: System.out.println(primeTester.testNumber(parameter) ? "PRIME" : "NO PRIME");
+                    case 3: System.out.println(palindromeTester.testNumber(parameter) ? "PALINDROME" : "NO PALINDROME");
+            }
+            }
+        }catch (IOException e){
+            System.out.println("Error in Method 'testFile()'!");
+        }
     }
 }
