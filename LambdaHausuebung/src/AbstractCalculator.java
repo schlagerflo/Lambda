4fc5037f397482f550e99bea181d01277f700a8f
Florian Schlager <schlagerf22108@sus.htl-grieskirchen.at>
2024-09-30 17:21:41 +0200

    Abstrakte Klasse AbstractCalculator fertiggestellt

--- LambdaHausuebung/src/AbstractCalculator.java
+++ LambdaHausuebung/src/AbstractCalculator.java
@@ -1,2 +1,18 @@
 public abstract class AbstractCalculator {
+    protected CalculationOperation add;
+    protected CalculationOperation subtract;
+    protected CalculationOperation multiply;
+    protected CalculationOperation divide;
+
+    public AbstractCalculator(CalculationOperation add, CalculationOperation subtract, CalculationOperation multiply, CalculationOperation divide) {
+        this.add = add;
+        this.subtract = subtract;
+        this.multiply = multiply;
+        this.divide = divide;
+    }
+
+    public abstract Number add (Number a, Number b);
+    public abstract Number subtract (Number a, Number b);
+    public abstract Number multiply (Number a, Number b);
+    public abstract Number divide (Number a, Number b);
 }
