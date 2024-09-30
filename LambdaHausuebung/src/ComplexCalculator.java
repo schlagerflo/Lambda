df84d943f9b9eb89cda7bde89caf6464941c42fa
Florian Schlager <schlagerf22108@sus.htl-grieskirchen.at>
2024-09-30 18:13:03 +0200

    File.txt an den richtigen Ort gespeichert & Calculator Klassen nahezu geschafft

--- LambdaHausuebung/src/ComplexCalculator.java
+++ LambdaHausuebung/src/ComplexCalculator.java
@@ -1,3 +1,26 @@
-public class ComplexCalculator {
-    
+public class ComplexCalculator extends AbstractCalculator{
+
+    public ComplexCalculator(CalculationOperation add, CalculationOperation subtract, CalculationOperation multiply, CalculationOperation divide) {
+        super(add, subtract, multiply, divide);
+    }
+
+    @Override
+    public Number add(Number a, Number b) {
+        return null;
+    }
+
+    @Override
+    public Number subtract(Number a, Number b) {
+        return null;
+    }
+
+    @Override
+    public Number multiply(Number a, Number b) {
+        return null;
+    }
+
+    @Override
+    public Number divide(Number a, Number b) {
+        return null;
+    }
 }
--- LambdaHausuebung/src/VectorCalculator.java
+++ LambdaHausuebung/src/VectorCalculator.java
@@ -1,3 +1,26 @@
-public class VectorCalculator {
+public class VectorCalculator extends AbstractCalculator{
 
+    public VectorCalculator(CalculationOperation add, CalculationOperation subtract, CalculationOperation multiply, CalculationOperation divide) {
+        super(add, subtract, multiply, divide);
+    }
+
+    @Override
+    public Number add(Number a, Number b) {
+        return null;
+    }
+
+    @Override
+    public Number subtract(Number a, Number b) {
+        return null;
+    }
+
+    @Override
+    public Number multiply(Number a, Number b) {
+        return null;
+    }
+
+    @Override
+    public Number divide(Number a, Number b) {
+        return null;
+    }
 }
