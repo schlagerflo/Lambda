4351b67c977ab90fb579aff59ff004af2d049781
Florian Schlager <schlagerf22108@sus.htl-grieskirchen.at>
2024-09-30 17:12:27 +0200

    Methode Number fertiggestellt

--- LambdaHausuebung/src/Number.java
+++ LambdaHausuebung/src/Number.java
@@ -1,3 +1,25 @@
 public class Number {
-    
+    private double a;
+    private double b;
+
+    public Number(double a, double b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public double getA() {
+        return a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public void setA(double a) {
+        this.a = a;
+    }
+
+    public void setB(double b) {
+        this.b = b;
+    }
 }
